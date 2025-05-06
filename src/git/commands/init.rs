ebf53976f04a49865eb7912b4950cf7b47c828fe
falehalnayef <alnayef.faleh@gmail.com>
2025-05-06 17:30:22 +0000

    test: implement tests for function: init .gut folder.

--- src/git/commands/init.rs
+++ src/git/commands/init.rs
@@ -1,14 +1,46 @@
+use std::io::{self, Write};
 use std::{fs, path::Path};
 
-pub fn init() {
-    let p = Path::new(".gut");
-    if p.exists() {
-        println!("already initialized");
-    } else {
-        fs::create_dir(".gut").expect("Failed to create .gut folder.");
-        fs::create_dir(".gut/objects").expect("Failed to create .gut/objects folder.");
-        fs::create_dir(".gut/refs").expect("Failed to create .gut/refs folder.");
-        fs::write(".gut/HEAD", "ref: refs/heads/main\n").expect("Failed to create .gut/HEAD flie.");
-        println!("Initialized gut directory");
+pub fn init() -> io::Result<()> {
+    let root = Path::new(".gut");
+
+    if root.exists() {
+        println!("Repository already initialized at {}", root.display());
+        return Ok(());
+    }
+
+    fs::create_dir_all(root.join("objects"))?;
+    fs::create_dir_all(root.join("refs"))?;
+
+    let head_path = root.join("HEAD");
+    fs::write(&head_path, "ref: refs/heads/main\n")?;
+
+    println!("Initialized empty .gut repository in {}", root.display());
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_check_gut_is_not_initialized() {
+        let _ = fs::remove_dir_all(".gut");
+        let path = Path::new(".gut");
+        assert!(!path.exists())
+    }
+
+    #[test]
+    fn test_initialize_gut() {
+        let res = init();
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn test_check_gut_folders_are_initialized() {
+        assert!(Path::new(".gut/objects").exists());
+        assert!(Path::new(".gut/refs").exists());
+        assert!(Path::new(".gut/HEAD").exists());
     }
 }
